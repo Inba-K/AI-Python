484410d5f48eb21f841b596f8b2e83617f55cd61
Inba-K <inba.karthikeyan18@gmail.com>
2026-07-20 15:14:33 -0700

    Rule based chatbot
    This is basically a rule based chatbot using python

---
+++ RuleBasedChatbot.py
@@ -0,0 +1,27 @@
+import datetime
+x=input()
+if 'hi' in x or 'Hi' in x or 'Hello' in x or 'hello' in x:
+    print("Hey!")
+a=input("What is your name? ")
+print("Hello",a,"! What can I help you with?")
+b=input()
+c="yes"
+while c=="yes":
+    if "time" in b:
+        print("No problem! The time is",datetime.datetime.now().time())
+    elif "date" in b:
+        print("Got it! The date is",datetime.datetime.now().date())
+    elif "weather" in b:
+        print("The weather is looking very hot today, as expected in July!")
+    elif "joke" in b:
+        print("Here's a joke! What do you call a bird without a beak?")
+        d=input()
+        if "ird" in d:
+            print("That's right!")
+        else:
+            print("An ird!")
+    print("What else can I help you with,",a,"?")
+    b=input()
+    if "nothing" in b:
+        print("Nice chatting,",a,"!")
+        break
